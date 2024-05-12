2d3167b2dc4380db83b447f56c98528ab4c3d031
avinashkrishnan2020 <avinashkrishnan2020@gmail.com>
2024-05-12 16:14:57 +0100

    new project files

---
+++ src/main/java/com/app/devblogservice/DevBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.devblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBlogServiceApplication.class, args);
+	}
+
+}
